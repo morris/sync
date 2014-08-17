b943451187ec6fb5b5597cfaa1521aa9471229a3
Morris Brodersen <morrisbrodersen@googlemail.com>
2014-08-17 18:02:35 +0200

    first commit

---
+++ src/Sync.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Provides a method to call synchronized functions, locking on files.
+ */
+class Sync {
+
+	/**
+	 * Default lock file
+	 */
+	public static $lock = '.lock';
+
+	/**
+	 * Call a function in a mutually exclusive way, locking on files.
+	 * A process will only block other processes and never block itself,
+	 * so you can safely nest synchronized operations.
+	 */
+	public static function call( $func, $lock = null ) {
+
+		// fall back to default lock
+
+		if ( !isset( $lock ) ) {
+
+			$lock = self::$lock;
+
+		}
+
+		// get absolute path
+
+		touch( $lock );
+		$lock = realpath( $lock );
+
+		// just call function if lock already acquired
+
+		if ( in_array( $lock, self::$locks ) ) {
+
+			return $func();
+
+		}
+
+		try {
+
+			// acquire lock
+
+			$handle = fopen( $lock, 'w' );
+
+			if ( !$handle || !flock( $handle, LOCK_EX ) ) {
+
+				throw new \Exception(
+
+					'Unable to synchronize over "'. $lock . '"'
+
+				);
+
+			}
+
+			self::$locks[ $lock ] = true;
+
+			// call function and release lock
+
+			$return = $func();
+
+			self::release( $lock, $handle );
+
+			return $return;
+
+		} catch ( \Exception $e ) {
+
+			// release lock and rethrow any exception
+
+			if ( isset( $handle ) ) {
+
+				self::release( $lock, $handle );
+
+			}
+
+			throw $e;
+
+		}
+
+	}
+
+	private static function release( $lock, $handle ) {
+
+		self::$locks[ $lock ] = false;
+
+		if ( $handle ) {
+
+			flock( $handle, LOCK_UN );
+			fclose( $handle );
+
+		}
+
+	}
+
+	// set of already acquired locks
+	private static $locks = array();
+
+}
---
+++ tests/SyncTest.php
@@ -0,0 +1,102 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+/**
+ * @runInSeparateProcess
+ */
+class DatabaseTest extends PHPUnit_Framework_TestCase {
+
+	static function setupBeforeClass() {
+
+		Sync::$lock = 'tests/.lock';
+
+	}
+
+	function testBasics() {
+
+		$a = array();
+
+		Sync::call( function() use ( &$a ) {
+
+			$a[] = 'called';
+
+			$handle = fopen( Sync::$lock, 'w+' );
+
+			if ( $handle && flock( $handle, LOCK_EX|LOCK_NB, $wouldblock ) ) {
+
+				flock( $handle, LOCK_UN );
+				fclose( $handle );
+
+			} else {
+
+				$a[] = 'locked';
+
+			}
+
+			if ( strtoupper( substr( PHP_OS, 0, 3 ) ) === 'WIN' ) {
+
+				// this fails on windows
+				// whereas LOCK_NB appears to be supported, at least by windows 8
+				// $this->assertEquals(1, $wouldblock);
+
+			} else {
+
+				$this->assertEquals( 1, $wouldblock );
+
+			}
+
+			Sync::call( function() use (&$a) {
+
+				$a[] = 'nested';
+
+			} );
+
+		} );
+
+		$ex = array( 'called', 'locked', 'nested' );
+
+		$this->assertEquals( $ex, $a );
+
+	}
+
+	function testIncrement() {
+
+		$self = $this;
+
+		Sync::call( function() use ( $self ) {
+
+			$file = 'tests/increment';
+
+			file_put_contents( $file, 0 );
+
+			for ( $i = 0; $i < 100; ++$i ) {
+
+				$j = intval( file_get_contents( $file ) );
+
+				$self->assertEquals( $i, $j );
+
+				file_put_contents( $file, $j + 1 );
+
+				usleep( rand( 100, 300) );
+
+			}
+
+		} );
+
+	}
+
+	/**
+     * @expectedException InvalidArgumentException
+     */
+	function testException() {
+
+		Sync::call( function() {
+
+			throw new \InvalidArgumentException( 'Oops' );
+
+		} );
+
+	}
+
+}
